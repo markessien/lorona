470dde68b70038f1d90e9fa9f1de8a258066bb5c
Mark Essien <markessien@gmail.com>
2020-12-10 14:17:24 +0100

    Added a separate settings file

--- lorona.go
+++ lorona.go
@@ -3,45 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-
-	"gopkg.in/yaml.v2"
 )
 
-// Config struct for webapp config
-type Settings struct {
-	Nginx struct {
-		Logfilename string `yaml:"log_filename"`
-	} `yaml:"nginx"`
-}
-
-func loadSettings(settingsFile string) (*Settings, error) {
-
-	settings := &Settings{}
-
-	// Open config file
-	file, err := os.Open(configPath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	// Init new YAML decode
-	d := yaml.NewDecoder(file)
-
-	// Start YAML decoding from file
-	if err := d.Decode(&settings); err != nil {
-		return nil, err
-	}
-
-	return settings, nil
-}
-
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Welcome to Lorona!")
 
-	settings, err := loadSettings("settings.yaml")
+	settings, err := LoadSettings("settings.yaml")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println("Lorona for package: " + settings.ContainerName)
 }
---
+++ settings.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+// Config struct for webapp config
+type Settings struct {
+	ContainerName        string `yaml:"container_name"`
+	ContainerSupport     string `yaml:"container_support"`
+	ContainerDescription string `yaml:"container_description"`
+
+	Nginx struct {
+		Logfilename string `yaml:"log_filename"`
+	} `yaml:"nginx"`
+
+	Uptime struct {
+		Endpoint           string `yaml:"endpoint"`
+		GeneralStatusCheck string `yaml:"general_status_check"`
+	} `yaml:"uptime"`
+}
+
+func LoadSettings(settingsFile string) (*Settings, error) {
+
+	settings := &Settings{}
+
+	// Open config file
+	file, err := os.Open(settingsFile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Init new YAML decode
+	d := yaml.NewDecoder(file)
+
+	// Start YAML decoding from file
+	if err := d.Decode(&settings); err != nil {
+		return nil, err
+	}
+
+	return settings, nil
+}
