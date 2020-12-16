2d0316da79f35c6210a4245b55a82e36ae889836
Mark Essien <markessien@gmail.com>
2020-12-16 20:18:24 +0100

    Added the tick at the correct time

--- monitor_backups.go
+++ monitor_backups.go
@@ -23,18 +23,38 @@ type BackupMonitorRequest struct {
 
 func StartBackupsMonitoring(settings *Settings, backups chan BackupInfo) error {
 
-	// WE use timers here. Each time they tick, we check how the backup situation
+	// We use timers here. Each time they tick, we check how the backup situation
 	// is currently looking. The timer ticks run in separate threads
 	for _, backupMonitorRequest := range settings.BackupMonitorRequest {
 
 		// Parse the time. The format is fixed in 24 hour time format (the 15:04 shows that)
 		t, err := time.Parse("15:04", backupMonitorRequest.CheckBackupTime)
 
-		fmt.Println(t.Format(time.RFC850))
+		// Get a datetime representing when the first check would happen today
+		// This is based on the requested backup check time
+		firstTick := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), t.Hour(), t.Minute(), 0, 0, time.UTC)
+
+		// Get the time the next tick would happen
+		nextTick := firstTick.Add(time.Hour * 24)
+
+		// Select if the first tick will be the tick today or tommorow
+		startTick := nextTick
+		if firstTick.After(time.Now()) {
+			// This means we will do a check today
+			startTick = firstTick
+		}
+
+		fmt.Println(firstTick.Format(time.RFC850))
+		fmt.Println(nextTick.Format(time.RFC850))
+		fmt.Println(startTick.Format(time.RFC850))
+
+		timeTillStart := time.Until(startTick)
+		fmt.Println(timeTillStart)
+
 		if err == nil {
 			if len(settings.BackupMonitorRequest) > 0 {
 				f := startBackupMonitoring(backupMonitorRequest.Folder)
-				_ = time.AfterFunc(1*time.Second, f)
+				_ = time.AfterFunc(timeTillStart, f)
 			}
 		}
 
@@ -51,8 +71,4 @@ func startBackupMonitoring(backupFolder string) func() {
 
 func checkForBackups(backupFolder string) {
 	fmt.Printf("creating func with '%s'\n", backupFolder)
-}
-
-func monitorBackups(settings *Settings, backups chan BackupInfo) {
-
 }
